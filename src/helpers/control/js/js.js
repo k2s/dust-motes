44fe343fb119b897856f18b831c433370329e80a
Martin Minka <martin.minka@gmail.com>
2014-03-06 18:01:44 +0100

    add jsEvalHandleUnknown method to handle unknown variables simillar @if does it is easy customize the helper to use either jsEvalPure or jsEvalHandleUnknown (template author can't decide at this moment)

--- src/helpers/control/js/js.js
+++ src/helpers/control/js/js.js
@@ -1,11 +1,16 @@
 /*globals dust, _console */
-(function(dust) {
-    dust.helpers.js = function(chunk, ctx, bodies, params) {
+(function (dust) {
+    dust.helpers.js = function (chunk, ctx, bodies, params) {
         // decide if we will use compiled expression cache stored in ctx.global
         var result, useCache = !!ctx.global.exprCache;
 
-        // execute JavaScript expression in right context
-        var jsEval = function(action) {
+        /**
+         * Evaluate expression in default JavaScript way
+         *
+         * @param action
+         * @returns {*}
+         */
+        var jsEvalPure = function (action) {
             var expr = dust.helpers.tap(params[action], chunk, ctx);
 
             try {
@@ -26,6 +31,53 @@
                 return undefined;
             }
         };
+
+        /**
+         * Evaluate expression with handling of unknown variable names
+         *
+         * @param action
+         * @returns {*}
+         */
+        var jsEvalHandleUnknown = function (action) {
+            var expr = dust.helpers.tap(params[action], chunk, ctx);
+            var unknownVars = {}, repeat;
+
+            do {
+                repeat = false;
+                try {
+                    // compile expression or use one cached in ctx.global
+                    var exprFunction;
+                    if (!repeat && useCache && ctx.global.exprCache[expr]) {
+                        exprFunction = ctx.global.exprCache[expr];
+                    } else {
+                        // build fix for JavaScript undefined exceptions
+                        var fixes = "var fixes_ = {};";
+                        for (var v in unknownVars) {
+                            fixes += 'if (typeof ' + v + ' ==="undefined") fixes_.' + v + '=null;';
+                        }
+                        fixes += "with(fixes_){"
+                        //noinspection JSHint
+                        exprFunction = new Function('ctx_', 'data_', "with (ctx_.global) with (data_) {" + fixes + 'return ' + expr + '}}');
+                        if (useCache) {
+                            ctx.global.exprCache[expr] = exprFunction;
+                        }
+                    }
+                    return exprFunction.call(chunk, ctx, ctx.stack.head);
+                } catch (e) {
+                    if (e.name === 'ReferenceError') {
+                        var a = e.message.match(/^(\w*)/);
+                        unknownVars[a[0]] = null;
+                        repeat = true;
+                    } else {
+                        _console.log('jsEvalToFunction (' + expr + ') EXCEPTION ' + e);
+                        return undefined;
+                    }
+                }
+            } while (repeat);
+        };
+
+        // decide what evaluation function to use
+        var jsEval = jsEvalHandleUnknown;
 
         // find out action type and process it
         if (params.hasOwnProperty('expr')) {
@@ -56,7 +108,11 @@
 
         } else if (params.hasOwnProperty('if')) {
             result = jsEval('if');
-            if (result) {
+            if (result === undefined && bodies.hasOwnProperty("unknown")) {
+                // @if returns empty string for unknown,
+                // but we do evaluate to false if :unknown block is not defined
+                return chunk.render(bodies['unknown'], ctx);
+            } else if (result) {
                 if (bodies.hasOwnProperty("block")) {
                     return chunk.render(bodies.block, ctx);
                 } else {
